8b765975dd3bc09ef1bdc068dda76c45782494ac
mihairu-code <167312496+mihairu-code@users.noreply.github.com>
2024-12-02 03:40:24 +0300

    fix routing

--- src/App.jsx
+++ src/App.jsx
@@ -17,25 +17,32 @@ export function App() {
     limit: 5,
     offset: 0,
   });
-  return (
-    <>
-      <Header />
+
+  if (isLoading) {
+    return (
       <main>
-        {/* eslint-disable-next-line no-nested-ternary */}
-        {isLoading ? (
-          <Skeleton className="loadingSpin" />
-        ) : error ? (
-          <div>Ошибка загрузки: {error.message || 'Неизвестная ошибка'}</div>
-        ) : (
-          <Routes>
-            <Route path="/" element={<ArticlesList />} />
-            <Route path="/articles" element={<ArticlesList />} />
-            <Route path="/articles/:slug" element={<OpenedArticle />} />
-            <Route path="/sign-in" element={<SignIn />} />
-            <Route path="/sign-up" element={<SignUp />} />
-          </Routes>
-        )}
+        <Skeleton className="loadingSpin" />
       </main>
-    </>
+    );
+  }
+
+  if (error) {
+    return (
+      <main>
+        <div>Ошибка загрузки: {error.message || 'Неизвестная ошибка'}</div>
+      </main>
+    );
+  }
+
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<ArticlesList />} />
+        <Route path="articles" element={<ArticlesList />} />
+        <Route path="articles/:slug" element={<OpenedArticle />} />
+        <Route path="sign-in" element={<SignIn />} />
+        <Route path="sign-up" element={<SignUp />} />
+      </Route>
+    </Routes>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,26 +1,31 @@
 import React from 'react';
 
 import { Button } from '@gravity-ui/uikit';
-import { Link } from 'react-router';
+import { Link, Outlet } from 'react-router';
 
 import '../styles/Header.less';
 
 export default function Header() {
   return (
-    <header className="header">
-      <Link className="header__title_link" to="/articles">
-        <h1 className="header__title">Realworld Blog</h1>
-      </Link>
-      <Link className="header__link margin_link" to="/sign-in">
-        <Button className="header__sign-in" view="flat" size="xl">
-          Sign In
-        </Button>
-      </Link>
-      <Link className="header__link" to="/sign-up">
-        <Button className="header__sign-up" view="outlined-success" size="xl">
-          Sign Up
-        </Button>
-      </Link>
-    </header>
+    <>
+      <header className="header">
+        <Link className="header__title_link" to="/articles">
+          <h1 className="header__title">Realworld Blog</h1>
+        </Link>
+        <Link className="header__link margin_link" to="/sign-in">
+          <Button className="header__sign-in" view="flat" size="xl">
+            Sign In
+          </Button>
+        </Link>
+        <Link className="header__link" to="/sign-up">
+          <Button className="header__sign-up" view="outlined-success" size="xl">
+            Sign Up
+          </Button>
+        </Link>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </>
   );
 }
