67cb3d18b58f7f2513c1b5c7bcb5accbb1157b19
mihairu-code <167312496+mihairu-code@users.noreply.github.com>
2024-12-08 13:47:00 +0300

    add func
    add users login state header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,62 @@
 import React from 'react';
 
-import { Button } from '@gravity-ui/uikit';
+import { Button, UserLabel } from '@gravity-ui/uikit';
 import { Link, Outlet } from 'react-router';
 
 import '../styles/Header.less';
 
 export default function Header() {
+  const a = '';
   return (
     <>
       <header className="header">
         <Link className="header__title_link" to="/articles">
           <h1 className="header__title">Realworld Blog</h1>
         </Link>
-        <Link className="header__link margin_link" to="/sign-in">
-          <Button className="header__sign-in" view="flat" size="xl">
-            Sign In
-          </Button>
-        </Link>
-        <Link className="header__link" to="/sign-up">
-          <Button className="header__sign-up" view="outlined-success" size="xl">
-            Sign Up
-          </Button>
-        </Link>
+        {a ? (
+          <>
+            <Link className="header__link margin_link" to="/sign-in">
+              <Button className="header__sign-in" view="flat" size="xl">
+                Sign In
+              </Button>
+            </Link>
+            <Link className="header__link" to="/sign-up">
+              <Button
+                className="header__sign-up"
+                view="outlined-success"
+                size="xl"
+              >
+                Sign Up
+              </Button>
+            </Link>
+          </>
+        ) : (
+          <>
+            <Link className="header__link margin_link" to="/new-article">
+              <Button
+                className="header__sign-in"
+                view="outlined-success"
+                size="m"
+              >
+                Create Article
+              </Button>
+            </Link>
+            <UserLabel
+              className="header__user-label"
+              type="person"
+              size="xl"
+              view="clear"
+              avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/3/33/Charles_Darwin_by_Julia_Margaret_Cameron%2C_c._1868.jpg/193px-Charles_Darwin_by_Julia_Margaret_Cameron%2C_c._1868.jpg"
+            >
+              User Name
+            </UserLabel>
+            <Link>
+              <Button className="header__logout" size="xl" view="outlined">
+                Log out
+              </Button>
+            </Link>
+          </>
+        )}
       </header>
       <main>
         <Outlet />
