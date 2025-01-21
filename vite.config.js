db9c61a0196ba322efa1cff3f44d17bf41b541db
mihairu-code <167312496+mihairu-code@users.noreply.github.com>
2025-01-21 06:53:25 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,6 +12,7 @@ export default defineConfig({
   base: '/',
   plugins: [react()],
   resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
     alias: {
       '@': path.resolve(__dirname, './src'),
       '@assets': path.resolve(__dirname, './src/assets'),
